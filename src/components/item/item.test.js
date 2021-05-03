4861c47a4b69fdc41a4d2daba4c1f1509b7abd6a
O. O. Nathan <nateoguns.work@gmail.com>
2021-05-03 21:20:05 +0100

    integrated item test

--- src/components/item/item.test.js
+++ src/components/item/item.test.js
@@ -6,26 +6,26 @@ import Item from "./item";
 
 let container = null;
 const config = {
-  actions: ['View', 'Edit', 'Delete'],
+  actions: ["View", "Edit", "Delete"],
   fields: [
     {
-      title: 'ID',
-      key: 'id',
+      title: "ID",
+      key: "id",
     },
     {
-      title: 'Full Name',
-      key: 'name',
+      title: "Full Name",
+      key: "name",
       isTitle: true,
     },
     {
-      title: 'Email Address',
-      key: 'email',
+      title: "Email Address",
+      key: "email",
       isTagline: true,
     },
     {
-      title: 'Date created',
-      key: 'date',
-      formatter: value => (new Date(value)).toDateString(),
+      title: "Date created",
+      key: "date",
+      formatter: (value) => new Date(value).toDateString(),
       isMetadata: true,
     },
   ],
@@ -35,7 +35,7 @@ const config = {
     email: "r.abdulkadir@gmail.com",
     date: Date.now(),
   },
-}
+};
 
 beforeEach(() => {
   container = document.createElement("tbody");
@@ -55,4 +55,42 @@ afterEach(() => {
 it("renders a line item:", () => {
   const itemElement = document.querySelector(".gm-table-line-item");
   expect(itemElement).not.toBeNull();
+});
+
+it("renders an item:", () => {
+  expect(container).toMatchInlineSnapshot(`
+    <tbody>
+      <tr
+        class="gm-table-line-item"
+      >
+        <td>
+          <input
+            type="checkbox"
+          />
+        </td>
+        <td>
+          1
+        </td>
+        <td>
+          Rahmon Abdulkadir
+        </td>
+        <td>
+          r.abdulkadir@gmail.com
+        </td>
+        <td>
+          Mon May 03 2021
+        </td>
+        <td>
+          <div
+            class="rd-menu-label"
+          >
+            Action
+          </div>
+          <div>
+            <div />
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  `);
 });
